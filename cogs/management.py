de9de1848c15be7054d757984d97013575f0ac32
KvantOrav <kvantorav@gmail.com>
2020-04-25 13:21:38 +0300

    fixed management, added ping filter

--- cogs/management.py
+++ cogs/management.py
@@ -10,11 +10,12 @@ class Management(commands.Cog, name='Management'):
     async def on_ready(self):
         print('loaded management')
     
-    @commands.has_any_role('Galacduck', 'Administrator')
+    @commands.has_any_role('Galacduck', 'Administrator', 'Moderator')
     @commands.command()
     async def send(self, ctx, channel, *, string):
-        msgchannelstr = discord.utils.get(self.bot.get_guild(414027124836532234).channels, name=channel)
-        await msgchannel.send(string)
+        msgchannel = discord.utils.get(self.bot.get_guild(414027124836532234).channels, name=channel)
+        if "@everyone" or "@here" not in string:
+            await msgchannel.send(string)
 
 
 def setup(bot):
