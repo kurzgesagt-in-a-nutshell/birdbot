a728db68dde63402dfc3bce1ce6c299a90d225db
KvantOrav <kvantorav@gmail.com>
2020-04-29 15:52:42 +0300

    fixes

--- cogs/fun.py
+++ cogs/fun.py
@@ -14,23 +14,21 @@ class Fun(commands.Cog, name='Fun'):
     async def on_ready(self):
         print('loaded fun')
 
-    @commands.guild_only()
     @commands.command()
     async def clap(self, ctx, *, clap):
         """Replaces spaces with :clap:"""
-        claps = re.sub(' +', ' ', clap).replace(" ", " :clap: ")
+        claps = ":clap: " + re.sub(' +', ' ', clap).replace(" ", " :clap: ") + " :clap:"
         await ctx.send(claps)
 
-    @commands.guild_only()
     @commands.command()
-    async def weather(self, ctx, city, units: Optional[str] = 'C'):
+    async def weather(self, ctx, *, city, units: Optional[str] = 'C'):
         """Shows weather in a city"""
         link=' http://api.openweathermap.org/data/2.5/weather?appid=3c3fdfdd08d48ebb5a66a27e376a719f&q='
         adr = link + city
         data = requests.get(adr).json()
         countrys = json.load(open('countries.json'))
         country = countrys[data["sys"]["country"]]
-        embed = discord.Embed(title = f"{country}, {city.title()}'s weather")
+        embed = discord.Embed(title = f"{city.title()}, {country}'s weather")
         val = f"{round(data['main']['temp']-273,1)} °C"
         val2 = f"{round(data['main']['feels_like']-273,1)} °C"
         if units.lower() == 'f':
