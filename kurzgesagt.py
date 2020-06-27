59d56ac812a4dd43f8168473e90b0f09b015890e
slothonmeth1 <siddharthn02@gmail.com>
2020-06-27 19:09:09 +0530

    change wrket

--- kurzgesagt.py
+++ kurzgesagt.py
@@ -1,35 +1,39 @@
 import time
 import os
+import logging
 import discord
 from discord.ext import commands
+logging.basicConfig(level=logging.INFO)
 
-bot = commands.Bot(command_prefix='k!')
-
-print("Hello!\nDelaying bot for (kinda) successful server creation.")
+print("Delaying bot for server creation")
 time.sleep(5)
+class Bot(commands.AutoShardedBot):
+    """Main Bot"""
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(f"\tUser: {bot.user.name}")
-    print(f"\tID  : {bot.user.id}")
-    print('------')
-    #bot status
-    type = discord.ActivityType.listening
-    activity = discord.Activity(type=type, name="to Steve's voice" )
-    await bot.change_presence(activity = activity)
+    def __init__(self):
+        bot = commands.Bot(command_prefix='k!',owner_ids={389718094270038018,183092910495891467,424843380342784011})
+        super().__init__(command_prefix="k!",case_insensitive=True,)
+        self.starttime = time.time()
+        cogs = []
+        fails = {}
+        for i in cogs:
+            try:
+                super().load_extension(i)
+                logger.info(f'Loading {i}')
+            except Exception as e:
+                logger.exception('Exception at {i}')
+                fails[i] = e
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        try:
-            bot.load_extension(f'cogs.{filename[:-3]}')
-        except Exception as error:
-            print("{0} cannot be loaded. [{1}]".format(
-                f'cogs.{filename[:-3]}', error))
-            print(''.join(traceback.TracebackException.from_exception(error).format()))
-        else:
-            loaded.append(f'cogs.{filename[:-3]}')
+        async def on_ready(self):
+            logger.info('Logged in as')
+            logger.info(f"\tUser: {bot.user.name}")
+            logger.info(f"\tID  : {bot.user.id}")
+            logger.info('------')
+            #bot status
+            activity = discord.Activity(type="discord.ActivityType.listening", name="to Steve's voice" )
+            await bot.change_presence(activity = activity)
 
 
-
-bot.run(open('token.txt').read(), reconnect=True)
+with open('token.txt') as tokenfile:
+    token = tokenfile.read()
+Bot().run(token)
