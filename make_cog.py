50435cfab7833812af29c1458a491e29ba18d41f
slothonmeth1 <siddharthn02@gmail.com>
2020-06-27 19:11:33 +0530

    cog draft

---
+++ make_cog.py
@@ -0,0 +1,30 @@
+name = input('Cog Name: ')
+text = f"""import logging
+
+import discord
+from discord.ext import commands
+
+
+class {''.join(name.title().split())}(commands.Cog):
+    def __init__(self, bot):
+        self.logger = logging.getLogger('{''.join(name.title().split())}')
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.logger.info('loaded {''.join(name.title().split())}')
+    
+    @commands.command()
+    async def your_command(self, ctx):
+        \"\"\"Command description\"\"\"
+        await ctx.send('thing')
+
+
+def setup(bot):
+    bot.add_cog({''.join(name.title().split())}(bot))
+
+"""
+f = open(f"cogs/{'_'.join(name.casefold().split())}.py", 'w+')
+f.write(text)
+f.close()
+print(f"Created cog with name {'_'.join(name.casefold().split())}.py in cogs directory")
