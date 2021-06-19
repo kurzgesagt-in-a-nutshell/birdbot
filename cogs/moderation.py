2af9c229dd4fc19c5cde648fcdb31ee44311f4c8
slothonmeth1 <siddharthn02@gmail.com>
2021-06-19 21:49:23 +0530

    Change clean arguments

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,6 +5,7 @@ import re
 import typing
 import time
 import asyncio
+import logging
 
 import helper
 from helper import helper_and_above, mod_and_above
@@ -34,76 +35,75 @@ class Moderation(commands.Cog):
 
     @commands.command(aliases=['purge', 'prune', 'clear'])
     @mod_and_above()
-    async def clean(self, ctx, msg_count: int = None, member: commands.Greedy[discord.Member] = None,
-                    channel: discord.TextChannel = None):
+    async def clean(self, ctx, member: commands.Greedy[discord.Member] = None,
+                    msg_count: int = None, channel: discord.TextChannel = None):
         """ Clean messages. \nUsage: clean number_of_messages <@member(s)/ id(s)> <#channel>"""
         try:
             messsage_count = msg_count #used to display number of messages deleted
             if msg_count is None:
-                return await ctx.send(f'**Usage:** `clean number_of_messages <@member(s)/ id(s)> <#channel>`')
+                return await ctx.send(f'**Usage:** `clean <@member(s)/ id(s)> number_of_messages <#channel>`')
 
-            elif msg_count > 200:
+            if msg_count > 200:
                 return await ctx.send(f'Provided number is too big. (Max limit 100)')
 
-            elif msg_count <= 0:
+            if msg_count <= 0:
                 return await ctx.channel.purge(limit=1)
 
+            if channel is None:
+                channel = ctx.channel
+
+            #check if message sent is by a user who is in command argument
+            def check(m):
+                if m.author in member:
+                    nonlocal messsage_count
+                    messsage_count -= 1
+                    if messsage_count >= 0:
+                        return True
+                return False
+
+            if not member:
+                deleted_messages = await channel.purge(limit=msg_count+1)
             else:
-                if channel is None:
-                    channel = ctx.channel
-                
-                #check if message sent is by a user who is in command argument
-                def check(m):
-                    if m.author in member:
-                        nonlocal messsage_count
-                        messsage_count -= 1
-                        if messsage_count >= 0:
-                            return True
-                    return False
+                deleted_messages = await channel.purge(limit=150, check=check)
 
-                if not member:
-                    deleted_messages = await channel.purge(limit=msg_count + 1)
-                else:
-                    deleted_messages = await channel.purge(limit=150, check=check)
+            await ctx.send(f"Deleted {len(deleted_messages) - 1} message(s)", delete_after=3.0)
 
-                await ctx.send(f"Deleted {len(deleted_messages) - 1} message(s)", delete_after=3.0)
+            if msg_count == 1:
+                logging_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
 
-                if msg_count == 1:
-                    logging_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
+                embed = helper.create_embed(author=ctx.author, users=None, action='1 message deleted',
+                                            extra=f"""Message Content: {deleted_messages[-1].content} 
+                                                  \nSender: {deleted_messages[-1].author.mention} 
+                                                  \nTime: {deleted_messages[-1].created_at.replace(microsecond=0)} 
+                                                  \nID: {deleted_messages[-1].id} 
+                                                  \nChannel: {channel.mention}""",
+                                            color=discord.Color.green())
 
-                    embed = helper.create_embed(author=ctx.author, users=None, action='1 message deleted',
-                                                extra=f"""Message Content: {deleted_messages[-1].content} 
-                                                      \nSender: {deleted_messages[-1].author.mention} 
-                                                      \nTime: {deleted_messages[-1].created_at.replace(microsecond=0)} 
-                                                      \nID: {deleted_messages[-1].id} 
-                                                      \nChannel: {channel.mention}""",
-                                                color=discord.Color.green())
+                await logging_channel.send(embed=embed)
 
-                    await logging_channel.send(embed=embed)
+            else:
+                #formatting string to be sent as file for logging
+                log_str = "Author (ID)".ljust(70) + " | " + "Message Creation Time (UTC)".ljust(
+                    30) + " | " + "Content" + "\n\n"
 
-                else:
-                    #formatting string to be sent as file for logging
-                    log_str = "Author (ID)".ljust(70) + " | " + "Message Creation Time (UTC)".ljust(
-                        30) + " | " + "Content" + "\n\n"
+                for msg in deleted_messages:
+                    author = f'{msg.author.name}#{msg.author.discriminator} ({msg.author.id})'.ljust(70)
+                    time = f'{msg.created_at.replace(microsecond=0)}'.ljust(30)
 
-                    for msg in deleted_messages:
-                        author = f'{msg.author.name}#{msg.author.discriminator} ({msg.author.id})'.ljust(70)
-                        time = f'{msg.created_at.replace(microsecond=0)}'.ljust(30)
+                    content = f'{msg.content}'
+                    #TODO save attachments and upload to logging channel
+                    if msg.attachments:
+                        content = "Attachment(s): "
+                        for a in msg.attachments:
+                            content = content + f'{a.filename}' + ", "
 
-                        content = f'{msg.content}'
+                    log_str = log_str + author + " | " + time + " | " + content + "\n"
 
-                        if msg.attachments:
-                            content = "Attachment(s): "
-                            for a in msg.attachments:
-                                content = content + f'{a.filename}' + ", "
+                logging_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
 
-                        log_str = log_str + author + " | " + time + " | " + content + "\n"
-
-                    logging_channel = discord.utils.get(ctx.guild.channels, id=self.logging_channel)
-
-                    await logging_channel.send(f'{len(deleted_messages)} messages deleted in {channel.mention}', 
-                                               file=discord.File(io.BytesIO(f'{log_str}'.encode()), 
-                                               filename=f'{len(deleted_messages)} messages deleted in {channel.name}'))
+                await logging_channel.send(f'{len(deleted_messages)} messages deleted in {channel.mention}',
+                                           file=discord.File(io.BytesIO(f'{log_str}'.encode()),
+                                           filename=f'{len(deleted_messages)} messages deleted in {channel.name}'))
 
         except Exception as e:
             self.logger.error(str(e))
