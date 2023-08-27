00721d030fb8ea1c50327d8acf68eebd099c6236
KvantOrav <64274282+KvantOrav@users.noreply.github.com>
2023-08-27 19:32:36 +0300

    staff intro with modals, wip

--- app/cogs/misc.py
+++ app/cogs/misc.py
@@ -3,6 +3,8 @@ import asyncio
 import json
 import re
 
+import aiohttp
+
 import demoji
 import pymongo
 
@@ -74,165 +76,205 @@ class Misc(commands.Cog):
         embed.set_author(name=member.display_name, icon_url=after.avatar.url)
         await msg.edit(embed=embed)
 
-    def parse_info(self, user_id, tz_text, bio, bird_icon):
-        """Get all the neccesary info and return an introduction embed"""
-        user = self.kgs_guild.get_member(user_id)
-        description = f"**{tz_text}**\n\n" + bio
-        footer_name = (
-            "Kurzgesagt Official"
-            if user.top_role.id == Reference.Roles.kgsofficial
-            else user.top_role.name
-        )
-        footer_icon = self.config["roleicons"][f"{user.top_role.id}"]
-        embed = discord.Embed(description=description, color=user.top_role.color)
-        embed.set_author(name=user.display_name, icon_url=user.avatar.url)
-        embed.set_footer(text=footer_name, icon_url=footer_icon)
-        embed.set_thumbnail(url=bird_icon)
-        return embed
 
-    async def reorder_intros(self, role, intro_channel) -> tuple:
-        """Deletes intros that are before role_id and returns list of tuples of the form
-        (mongodb.document, discord.Embed)"""
-        embeds = []
-        async for message in intro_channel.history():
-            if not message.embeds:
-                break
 
-            if message.embeds[0].footer.text == role.name:
-                break
-
-            doc = self.intro_db.find_one({"message_id": message.id})
-            embeds.append((doc, message.embeds[0]))
-            await message.delete()
-
-        return embeds
-
-    @checks.role_and_above(Reference.Roles.subreddit_mod)  # subreddit mods and above | exludes trainees
-    @commands.group(hidden=True)
-    async def intro(self, ctx):
+    @app_commands.command()
+    @checks.devs_only()
+    async def intro_modal(self, interaction: discord.Interaction):
         """
         Staff intro commands
-        Usage: intro add/edit
-        """
+        Usage: intro
+        """        
+        oldIntro = self.intro_db.find_one({"_id": interaction.user.id})
+        await interaction.response.send_modal(introModal(oldIntro=oldIntro, bot = self.bot))
 
-    @intro.command()
-    async def add(self, ctx):
 
-        self.kgs_guild = self.bot.get_guild(Reference.guild)
-
-        intro = self.intro_db.find_one({"_id": ctx.author.id})
-        if intro:
-            await ctx.send(
-                "It looks like you already have an intro. Use '!intro edit' to make changes to it"
-            )
-            return
-
-        def check(m):
-            return m.channel == ctx.channel and m.author == ctx.author
-
-        try:
-            await ctx.send(
-                "Enter your timezone. This info need not neccesarily be accurate and can be memed."
-            )
-            tz_text = await self.bot.wait_for("message", check=check, timeout=180)
-            await ctx.send("Okay, Enter your bio")
-            bio = await self.bot.wait_for("message", check=check, timeout=240)
-            await ctx.send(
-                "Neat bio. Now give me the image link for your personal bird. The image should be fully transparent"
-            )
-            img = await self.bot.wait_for("message", check=check, timeout=240)
-            if not img.content.startswith("http"):  # dont wanna use regex
-                await ctx.send(
-                    "That does not appear to be a valid link. Please run the command again"
-                )
-                return
-        except asyncio.TimeoutError:
-            await ctx.send(
-                "You took too long to respond :(\nPlease run the command again"
-            )
-            return
+class introModal(discord.ui.Modal):
+    """
+    The modal UI for intro commands.
+    """
+    def __init__(self, oldIntro, bot):
+        super().__init__(title="Introduce yourself!")
+        self.oldIntro = oldIntro
+        #we need to have the bot!
+        self.bot = bot
+        self.intro_db = bot.db.StaffIntros
+        # require fields if intro is empty and set up placeholders/default values
+        timezone_ph = bio_ph = image_ph = None
+        timezone_default = bio_default = image_default = None
+        if oldIntro:
+            timezone_default  = oldIntro["tz_text"]
+            bio_default = oldIntro["bio"]
+            image_default = oldIntro["image"]
+            required = False
+            #we are doing this for error handling or in case the mongo entry remained incomplete
+            if oldIntro["message_id"] == None:
+                required = True
         else:
-            embed = self.parse_info(
-                ctx.author.id, tz_text.content, bio.content, img.content
-            )
-            intro_channel = self.kgs_guild.get_channel(
-                Reference.Channels.intro_channel
+            timezone_ph = "The internet - UTC | GMT+0:00"
+            bio_ph = "Hello! I'm Birdbot and I help run this server."
+            image_ph = "https://cdn.discordapp.com/avatars/471705718957801483/cfcf7fbcdc9579d7f0606b014aa1ede8.png"
+            required = True
+
+        self.required = required
+        
+        self.timezone = discord.ui.TextInput(label='Enter your timezone.', style=discord.TextStyle.short, required=required, placeholder=timezone_ph, default = timezone_default, max_length=90)
+
+        self.bio = discord.ui.TextInput(label='Enter your bio.', style=discord.TextStyle.paragraph, required=required, placeholder=bio_ph, default=bio_default)
+
+        self.image = discord.ui.TextInput(label='Enter the image link for your personal bird.', style=discord.TextStyle.short, required=required, placeholder=image_ph, default=image_default)
+
+        self.add_item(self.timezone)
+        self.add_item(self.bio)
+        self.add_item(self.image)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        #check the validity of the image link, is it worth it to import aiohttp for this?
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.image.value) as response:
+                    if response.content_type.startswith("image"):
+                        pass
+                    else:
+                        raise commands.BadArgument
+        except:
+            raise commands.BadArgument
+
+        #we're going to need these
+        kgs_guild = self.bot.get_guild(Reference.guild)
+        user = kgs_guild.get_member(interaction.user.id)
+        role = user.top_role
+        intro_channel = kgs_guild.get_channel(
+            Reference.Channels.intro_channel
+        )
+
+        if self.required:
+            """We are adding a new intro so make an introduction embed"""
+
+            description = f"**{self.timezone.value}**\n\n" + self.bio.value
+
+            footer_name = (
+                "Kurzgesagt Official"
+                if role.id == Reference.Roles.kgsofficial
+                else role.name
             )
 
-            embeds_to_add = await self.reorder_intros(
-                ctx.author.top_role, intro_channel
-            )
-            msg = await intro_channel.send(embed=embed)
-            self.intro_db.insert_one(
+            footer_icon = role.display_icon #this can return None if theres no icon
+            #if role has unicode emojis, dont. not a problem in our server but just in case
+            if not footer_icon.url:
+                footer_icon = None
+
+            embed = discord.Embed(description=description, color=role.color)
+            embed.set_author(name=user.display_name, icon_url=user.avatar.url)
+            embed.set_footer(text=footer_name, icon_url=footer_icon)
+            embed.set_thumbnail(url=self.image.value)
+
+            self.bot.intro_db.insert_one(
                 {
-                    "_id": ctx.author.id,
-                    "tz_text": tz_text.content,
-                    "bio": bio.content,
-                    "message_id": msg.id,
+                    "_id": interaction.user.id,
+                    "tz_text": self.timezone.value,
+                    "bio": self.bio.value,
+                    "message_id": None,  #we will edit it with message id after reordering
+                    "image": self.image.value
                 }
             )
 
-            if embeds_to_add:
-                for doc, embed in embeds_to_add:
+            #the command user wants some feedback
+            message = f'Your intro will be added! (not really, just testing)'
+
+        else:
+            """We are editing an existing intro"""
+
+            msg = await intro_channel.fetch_message(self.oldIntro["message_id"])
+            embed = msg.embeds[0]
+
+            #edit the embed
+            embed.description = f"**{self.timezone.value}**\n\n" + self.bio.value
+            embed.set_thumbnail(url=self.image.value)
+
+            #do we need to edit the footer too?
+            if embed.footer.text == role.name or role.name == "Kurzgesagt Official":
+                await msg.edit(embed=embed)
+                self.intro_db.update_one(
+                    {"_id": interaction.user.id},
+                    {"$set": {"tz_text": self.timezone.value, "bio": self.bio.value, "image": self.image.value}},
+                )
+            else:
+                #the user got promoted or demoted
+                footer_name = (
+                    "Kurzgesagt Official"
+                    if role.id == Reference.Roles.kgsofficial
+                    else role.name
+                )
+                embed.color = role.color
+
+                #we are just reusing a variable to reorder
+                self.required = True
+
+
+            #the command user wants some feedback
+            message = f'Your intro will be edited! (not really, just testing)'
+
+        #reorder!
+        if self.required:        
+            """Deletes intros that are before role_id and makes a list of tuples of the form
+            (mongodb.document, discord.Embed)"""
+
+            embeds = []
+            async for message in intro_channel.history():
+                if not message.embeds:
+                    break
+
+                if message.embeds[0].footer.text == role.name:
+                    break
+
+                doc = self.intro_db.find_one({"message_id": message.id})
+                embeds.append((doc, message.embeds[0]))
+                await message.delete()
+
+            #now we can send the new intro message and add to mongo
+            msg = await intro_channel.send(embed=embed)
+            self.intro_db.update_one(
+                {"_id": user.id["_id"]}, {"$set": {"message_id": msg.id}}
+            )
+
+            #add the deleted embeds back
+            if embeds:
+                for doc, embed in embeds:
                     msg = await intro_channel.send(embed=embed)
                     self.intro_db.update_one(
                         {"_id": doc["_id"]}, {"$set": {"message_id": msg.id}}
                     )
 
-            await ctx.send("Success.")
 
-    @intro.command()
-    async def edit(self, ctx):
+        #lets add server emojis, because modals dont support them
+        serverEmojis = [f":{emoji.name}:" for emoji in kgs_guild.emojis]
+        serverEmojiIds = [emoji.id for emoji in kgs_guild.emojis]
 
-        self.kgs_guild = self.bot.get_guild(Reference.guild)
-        intro = self.intro_db.find_one({"_id": ctx.author.id})
-        if not intro:
-            await ctx.send(
-                "It looks like you dont have an intro. Use '!intro add' to add one"
-            )
-            return
+        embed.description = re.sub(r"(?<!<):[A-Za-z0-9_.]+:(?![0-9]+>)", lambda x: f"<{x.group()}{serverEmojiIds[serverEmojis.index(x.group())]}>" if x.group() in serverEmojis else x.group(), embed.description)
 
-        def check(m):
-            return m.channel == ctx.channel and m.author == ctx.author
 
-        try:
-            await ctx.send(
-                "Enter your timezone. This info need not neccesarily be accurate and can be memed.\nType `skip` if you want to leave this unchanged"
-            )
-            tz_text = await self.bot.wait_for("message", check=check, timeout=180)
-            if tz_text.content.lower() == "skip":
-                str_tz_text = intro["tz_text"]
-            else:
-                str_tz_text = tz_text.content
+        await interaction.channel.send(embed=embed)
 
-            await ctx.send(
-                "Okay, Enter your bio. Type 'skip' if you want to leave this unchanged"
-            )
-            bio = await self.bot.wait_for("message", check=check, timeout=240)
-            if bio.content.lower() == "skip":
-                str_bio = intro["bio"]
-            else:
-                str_bio = bio.content
+        #give the command user some feedback
+        await interaction.response.send_message(message, ephemeral=True)
 
-        except asyncio.TimeoutError:
-            await ctx.send(
-                "You took too long to respond :( Please run the command again"
-            )
-            return
+    async def on_error(self, interaction: discord.Interaction, error: Exception):
+        if isinstance(error, commands.BadArgument):
+            #in the future actually do this properly
+            #save the users inputs, this doc doesn't need more data
+            '''currentState = {
+                    "tz_text": self.timezone.value,
+                    "bio": self.bio.value,
+                    "image": "Incorrect image link"
+                }'''
+            #discordpy is not happy with sending a modal like this, look into it in the future?
+            #await interaction.response.send_modal(introModal(oldIntro=currentState, bot = self.bot))
+            #im just closing the modal without saving right now
+            await interaction.response.send_message("Incorrect image link, sorry", ephemeral=True)
         else:
-            intro_channel = self.kgs_guild.get_channel(
-                Reference.Channels.intro_channel
-            )
-            msg = await intro_channel.fetch_message(intro["message_id"])
-            embed = msg.embeds[0]
-            embed.description = f"**{str_tz_text}**\n\n" + str_bio
-            await msg.edit(embed=embed)
+            raise error
 
-            self.intro_db.update_one(
-                {"_id": ctx.author.id},
-                {"$set": {"tz_text": str_tz_text, "bio": str_bio}},
-            )
-            await ctx.send("Success.")
 
 
     @app_commands.command()
