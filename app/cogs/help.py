c49f735e953f4a09f1146d2f4947886e239ab161
arcinfini <arcinfini@arcinfini.net>
2023-10-17 15:58:14 -0400

    main feature

--- app/cogs/help.py
+++ app/cogs/help.py
@@ -1,4 +1,4 @@
-import logging
+import logging, re, asyncio
 
 import discord
 from discord import Interaction, app_commands, ui as dui
@@ -11,34 +11,93 @@ from app.utils import checks
 from app.utils.config import Reference
 from app.utils.helper import get_active_staff
 
+logger = logging.getLogger(__name__)
+
 class ReportView(dui.View):
     """
     Handles the interactions with the buttons below a report embed as well as
     the continual pestering of active reports.
     """
 
-    def __init__(self, *, resolve_id:str, claim_id:str):
+    def __init__(self, *, bot: BirdBot, resolve_id:str, claim_id:str):
         super().__init__(timeout=None)
+
+        self.bot = bot
 
         self._resolve.custom_id = resolve_id
         self._claim.custom_id = claim_id
 
-        self.active_reports = []
+        self.active_reports: list[int] = []
 
-    async def insert_issue(self):
+        self.resolve_lock = asyncio.Lock()
+
+        self._reminder_task.start()
+
+    def insert_issue(self, message: discord.Message):
         """
         Inserts a new issue into the active issues
         """
         
-        pass
+        self.active_reports.append(message.id)
+
+    def collect_claimed(self, embed: discord.Embed) -> list[int]:
+        """
+        Collects a list of users who have claimed the issue
+        """
+        
+        footer = embed.footer.text
+        if footer is None: return []
+
+        matches = re.findall(r"([\d]+)", footer)
+
+        return [int(i) for i in matches]
     
     @dui.button(label="Mark as resolved")
     async def _resolve(self, interaction: Interaction, button: dui.Button):
         """
         Marks the embed as resolved and removes the report from the ping cycle.
+
+        DMs the reporter that the report has been marked as resolved.
         """
-        
-        pass
+        await interaction.response.defer(ephemeral=True)
+
+        async with self.resolve_lock:
+            if (message := interaction.message) is None \
+                or (embed := message.embeds[0]) is None: 
+                return
+            
+            if message.id not in self.active_reports:
+                await interaction.followup.send(
+                    "This issue has already been marked as resolved",
+                    ephemeral=True
+                )
+                return
+            
+            user = interaction.user
+
+            # Update the embed
+
+            embed.color = 0x00FF00
+            embed.set_footer(
+                text=f"Marked resolved by {user.name}({user.id})", 
+                icon_url=user.display_avatar.url
+            )
+
+            self.active_reports.remove(message.id)
+            await interaction.followup.edit_message(
+                message.id,
+                embed = embed, view = None
+            )
+
+        # finally once we are out of the lock, DM the user who reported
+
+        """
+        Your report has been marked as resolved
+        *Provide report content*
+
+        If you think this is not the case, please submit a new report with more
+        detailed information.
+        """
 
     @dui.button(label="Claim this issue")
     async def _claim(self, interaction: Interaction, button: dui.Button):
@@ -50,14 +109,90 @@ class ReportView(dui.View):
         15 minutes rather than every 5
         """
 
-    @tasks.loop()
-    async def _reminder_task(self,):
+        if (message := interaction.message) is None \
+            or (embed := message.embeds[0]) is None: 
+            return
+        
+
+        claimed = self.collect_claimed(embed)
+
+        if interaction.user.id in claimed:
+            await interaction.response.send_message(
+                "You have already claimed this issue",
+                ephemeral=True
+            )
+            return
+
+        claimed.append(interaction.user.id)
+
+        embed.set_footer(text=f"Claimed: ({str(claimed).strip('[]')})")
+
+        await interaction.response.edit_message(embed=embed)
+
+    def stop(self):
         """
-        Checks periodically for reports that have not been dealt with.
+        Stops the view and the task
         """
         
+        super().stop()
+
+        self._reminder_task.stop()
+
+    @tasks.loop(minutes = 5)
+    async def _reminder_task(self):
+        """
+        Checks periodically for reports that have not been dealt with.
+
+        Reports that are unclaimed are reminded about each iteration.
+        Reports that are claimed and unresolved are reminded about each 3
+        iterations
+        """
+        unresolved:list[discord.Message] = []
+        in_progress:list[discord.Message] = []
+
+        logger.debug("Report Reminder task begins")
+        logger.debug(self._reminder_task.current_loop)
+
+        mod_channel = self.bot.get_channel(Reference.Channels.mod_chat)
+        if not isinstance(mod_channel, discord.TextChannel): 
+            raise Exception("Mod chat is appearing as a non text channel")
+        
         for report in self.active_reports:
-            pass
+            logger.debug("Report Reminder Task report iteration")
+            try:
+                message = await mod_channel.fetch_message(report)
+                embed = message.embeds[0]
+                
+                claimed = self.collect_claimed(embed)
+                to_add = in_progress if len(claimed) > 0 else unresolved
+
+                to_add.append(message)
+            except discord.NotFound:
+                # Removes the reference if the message is not found
+
+                self.active_reports.remove(report)
+
+        if len(unresolved) == 0 and len(in_progress) == 0:
+            logger.debug("Report Reminder no active reports to remind")
+            return
+        
+        content = ", ".join([m.jump_url for m in unresolved]) + "\n" + \
+            ", ".join([m.jump_url for m in in_progress])
+        
+        await mod_channel.send(content)
+
+    @_reminder_task.before_loop
+    async def _before_reminder(self):
+        """
+        This needs to be done only to satisfy requirements for the first loop
+        and for following loops when the bot may not be actively connected to
+        discord
+        """
+        
+        await self.bot.wait_until_ready()
+        
+        
+
 
 class ReportModal(dui.Modal):
     text = dui.TextInput(
@@ -65,15 +200,16 @@ class ReportModal(dui.Modal):
         style=discord.TextStyle.long,
     )
     message = dui.TextInput(
-        label="Provide a link to a relevant message if helpful:",
+        label="Helpful discord link if any:",
         style=discord.TextStyle.short,
         placeholder="https://discord.com/channels/414027124836532234/414268041787080708/937750299165208607",
         required=False,
     )
 
-    def __init__(self, member):
+    def __init__(self, member, report_view: ReportView):
         super().__init__(title="Report")
         self.member = member
+        self.report_view = report_view
 
     def build_embed(
             self, 
@@ -102,7 +238,7 @@ class ReportModal(dui.Modal):
     async def on_submit(self, interaction: discord.Interaction):
         """
         Validates the user input where needed and provides a report embed in the
-        moderation chat
+        moderation chat.
         """
         
         message_link = self.message.value
@@ -123,11 +259,14 @@ class ReportModal(dui.Modal):
         if not isinstance(mod_channel, discord.TextChannel): 
             raise Exception("The destination of the report does not exist")
 
-        await mod_channel.send(
+        report_message = await mod_channel.send(
             get_active_staff(interaction.client),
             embed=self.build_embed(interaction, channel_mention, message_link),
-            allowed_mentions=discord.AllowedMentions.all(),
+            view=self.report_view,
+            allowed_mentions=discord.AllowedMentions.none(),
         )
+
+        self.report_view.insert_issue(report_message)
 
         await interaction.response.send_message("Your report has been sent!", ephemeral=True)
 
@@ -137,9 +276,27 @@ class Help(commands.Cog):
         self.bot = bot
         self.bot.remove_command("help")
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.logger.info("loaded Help")
+    async def cog_load(self):
+        """
+        Creates the ReportView class Object and begins listening to new events
+        """
+
+        buser = self.bot._user()
+        
+        self.REPORT_VIEW = ReportView(
+            bot = self.bot,
+            resolve_id=f"REPORT-RESOLVE-{buser.id}",
+            claim_id=f"REPORT-CLAIM-{buser.id}",
+        )
+
+        self.bot.add_view(self.REPORT_VIEW)
+
+    async def cog_unload(self) -> None:
+        """
+        Stops both the ReportView listening and task loop within the ReportView
+        """
+        
+        self.REPORT_VIEW.stop()
 
     # TODO: Convert the output to embed or some UI
     # TODO: Remove mod_and_above and default_permission check.
@@ -230,9 +387,10 @@ class Help(commands.Cog):
         member: discord.Member
             Mention or ID of member to report (is optional)
         """
-        mod_channel = self.bot._get_channel(Reference.Channels.mod_chat)
 
-        await interaction.response.send_modal(ReportModal(member=member))
+        await interaction.response.send_modal(
+            ReportModal(member=member, report_view=self.REPORT_VIEW)
+        )
 
 
 async def setup(bot: BirdBot):
